1aac988c85768d2f59a3c5c1aa574bec0ee19bb1
MJay1123 <tjalswhd1@naver.com>
2025-02-07 18:24:27 +0900

    baekjoon coding study repository 생성

---
+++ src/main/java/baekjoon/week01/test2.java
@@ -0,0 +1,28 @@
+package baekjoon.week01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class test2 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(n);
+        for (int i = 0; i < n; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }
+    }
+
+}
