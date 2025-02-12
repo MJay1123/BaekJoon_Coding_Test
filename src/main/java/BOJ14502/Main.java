086991dd454462964dc6e62a0c481f7762dfa52a
MJay1123 <tjalswhd1@naver.com>
2025-02-12 19:56:57 +0900

    25.02.12 문제풀이

---
+++ src/main/java/BOJ14502/Main.java
@@ -0,0 +1,89 @@
+package BOJ14502;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static List<Integer> virusRowList = new ArrayList<>();
+    static List<Integer> virusColumnList = new ArrayList<>();
+    static List<Boolean> virusBooleanList = new ArrayList<>();
+    static List<Integer> emptyRowList = new ArrayList<>();
+    static List<Integer> emptyColumnList = new ArrayList<>();
+    static List<Boolean> emptyBooleanList = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N][M];
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<M; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i=0; i<N; i++){
+            System.out.println(Arrays.toString(arr[i]));
+        }
+    }
+
+    public static void dfs(int r, int c, int[][] arr){
+        arr[r][c] = 2;
+        if(r+1 < arr.length){
+            if(arr[r+1][c] == 0){
+                dfs(r+1, c, arr);
+            }
+        }
+        if(r-1 >= 0){
+            if(arr[r-1][c] == 0){
+                dfs(r-1, c, arr);
+            }
+        }
+        if(c+1 < arr[0].length){
+            if(arr[r][c+1] == 0){
+                dfs(r, c+1, arr);
+            }
+        }
+        if(c-1 >= 0){
+            if(arr[r][c-1] == 0){
+                dfs(r, c-1, arr);
+            }
+        }
+    }
+
+    public static void getEmptyGraph(int[][] arr){
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                if(arr[i][j] == 2){
+                    emptyRowList.add(i);
+                    emptyColumnList.add(j);
+                    emptyBooleanList.add(false);
+                }
+            }
+        }
+    }
+
+    public static void getVirusGraph(int[][] arr){
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                if(arr[i][j] == 2){
+                    virusRowList.add(i);
+                    virusColumnList.add(j);
+                    virusBooleanList.add(false);
+                }
+            }
+        }
+    }
+
+}
---
+++ src/main/java/BOJ2909/Main.java
@@ -0,0 +1,24 @@
+package BOJ2909;
+// 캔디 구매
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int C = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        // (0 ≤ C ≤ 1,000,000,000, 0 ≤ K ≤ 9)
+
+        int money = (int) Math.pow(10,  K);
+        double a = 1.0 * C / money;
+        System.out.println(money * Math.round(a));
+
+    }
+}
