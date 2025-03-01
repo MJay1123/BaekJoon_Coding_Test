1e4ed1c41eb076956e62e90f6a80f66faedb92ee
MJay1123 <tjalswhd1@naver.com>
2025-03-01 22:12:29 +0900

    25.03.01 문제풀이 업로드

--- src/main/java/G5/BOJ7576/Main.java
+++ src/main/java/G5/BOJ7576/Main.java
@@ -1,10 +1,7 @@
-package notyet.BOJ7576;
+package G5.BOJ7576;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 // 토마토
 public class Main {
@@ -13,8 +10,8 @@ public class Main {
     static int N;
     static int[][] box;
     static boolean[][] visited;
-    static boolean[][] bfsDid;
     static int[][] move = {{1,0},{-1,0},{0,1},{0,-1}};
+    static List<Location> tomatoes = new ArrayList<>();     // 익은 토마토 위치
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,38 +22,34 @@ public class Main {
         M = Integer.parseInt(st.nextToken());       // M : 상자의 가로 길이
         N = Integer.parseInt(st.nextToken());       // N : 상자의 세로 길이
         box = new int[N][M];
-        bfsDid = new boolean[N][M];                 // bfs를 여러 번 진행하면서 최단시간을 구해야 함
+
 
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<M; j++){
                 box[i][j] = Integer.parseInt(st.nextToken());
+                if(box[i][j] == 1){
+                    tomatoes.add(new Location(i, j));       // 익은 토마토 위치
+                }
             }
         }
 
         // 정수 1은 익은 토마토, 정수 0은 익지 않은 토마토, 정수 -1은 토마토가 들어있지 않은 칸
         // 토마토가 모두 익을 때까지의 최소 날짜를 출력
-
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                if(box[i][j] == 1 && !bfsDid[i][j]){        // bfs를 여러번 하기 위함
-                    visited = new boolean[N][M];            // 방문 배열 초기화
-                    bfs(new Location(i,j));
-                }
-            }
-        }
-
-        for(int i=0; i<N; i++){
-            System.out.println(Arrays.toString(box[i]));
-        }
+        visited = new boolean[N][M];
+        bfs();
 
         boolean tomatoNot = false;
         int answer = 0;
         for(int i=0; i<N; i++){
+            if(tomatoNot){
+                break;
+            }
             for(int j=0; j<M; j++){
                 answer = Math.max(answer, box[i][j]);
                 if(box[i][j] == 0){                         // 안 익은 토마토 존재
                     tomatoNot = true;
+                    break;
                 }
             }
         }
@@ -68,27 +61,29 @@ public class Main {
         bw.flush();
     }
 
-    public static void bfs(Location start){
+    public static void bfs(){
         Queue<Location> queue = new LinkedList<>();
-        bfsDid[start.x][start.y] = true;
-        visited[start.x][start.y] = true;
-        queue.offer(new Location(start.x, start.y));
+        int x=0;
+        int y=0;
+
+        for(Location loc : tomatoes){
+            queue.add(loc);
+            x = loc.x;
+            y = loc.y;
+            visited[x][y] = true;
+        }
 
         while(!queue.isEmpty()) {
             Location loc = queue.poll();
-            int x = loc.x;
-            int y = loc.y;
+            x = loc.x;
+            y = loc.y;
             for (int i = 0; i < 4; i++) {
                 int nx = x + move[i][0];
                 int ny = y + move[i][1];
                 if (checkRange(nx, ny) && !visited[nx][ny] && box[nx][ny] != -1) {  // 빈 칸은 pass
                     queue.offer(new Location(nx, ny));
                     visited[nx][ny] = true;
-                    if (box[nx][ny] == 0) {
-                        box[nx][ny] = box[x][y] + 1;                                // 0은 그대로 표시
-                    } else {
-                        box[nx][ny] = Math.min(box[nx][ny], box[x][y] + 1);         // 이미 값이 있다면 최솟값으로 변경
-                    }
+                    box[nx][ny] = box[x][y] + 1;
                 }
             }
         }
---
+++ src/main/java/S1/BOJ1697/Main.java
@@ -0,0 +1,46 @@
+package S1.BOJ1697;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 숨바꼭질
+public class Main {
+    static int N;           // 수빈
+    static int K;           // 동생
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        if(K <= N){
+            bw.write(N-K + "\n");
+        } else {
+
+            int[] array = new int[K + 10];
+
+            for (int i = 0; i < K + 10; i++) {
+                array[i] = Math.abs(N - i);                                 // 1칸씩 이동할 때 걸리는 시간
+            }
+            for (int i = N + 1; i < K + 10; i++) {
+                if (i % 2 == 0) {
+                    array[i] = Math.min(array[i], array[i / 2] + 1);        // 순간이동 시간으로 대체
+                }
+            }
+            for (int i = N + 1; i < K + 9; i++) {
+                if (i % 2 == 1) {
+                    array[i] = Math.min(Math.min(array[i - 1], array[i + 1]) + 1, array[i]);
+                }
+            }
+            for(int i=N+1; i<K+9; i++){
+                array[i] = Math.min(Math.min(array[i-1],array[i+1])+1, array[i]);
+            }
+            bw.write(array[K] + "\n");
+        }
+        bw.flush();
+    }
+}
