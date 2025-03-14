4b25a5935dff48f9f091c34e02dde172ee21c69e
MJay1123 <tjalswhd1@naver.com>
2025-03-14 12:40:59 +0900

    S4 올바른 배열 풀이

--- src/main/java/Silver/S4/BOJ1337/Main.java
+++ src/main/java/Silver/S4/BOJ1337/Main.java
@@ -20,22 +20,21 @@ public class Main {
             long num = Integer.parseInt(br.readLine());
             list.add(num);
         }
-        list.add(-1L);
-        list.add(-1L);
-        list.add(-1L);
-        list.add(-1L);
         Collections.sort(list);
-        for(int i=0; i<N-4; i++){
+        list.add(0L);
+        list.add(0L);
+        list.add(0L);
+        list.add(0L);
+//        System.out.println(list);
+        for(int i=0; i<N; i++){
             count = 1;
             for(int j=1; j<=4; j++){
                 if(list.get(i+j) - list.get(i) <= 4 && list.get(i+j) > 0){
                     count++;
-                    System.out.println(list.get(i+j));
-                    System.out.println(list.get(i));
-                    System.out.println("count = " + count);
                 }
                 max = Math.max(max, count);
             }
+//            System.out.println("count = " + count);
         }
 //        System.out.println("max = " + max);
         bw.write(5 - max + "\n");
