a43d0dee461234e09fc6798b44befd653b7601f6
= <tjalswhd1@naver.com>
2025-09-12 00:13:41 +0900

    25.09.12 문제풀이 업로드

---
+++ src/main/java/Bronze/B2/BOJ25305/Main.java
@@ -0,0 +1,27 @@
+package Bronze.B2.BOJ25305;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int k;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<N; i++){
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(list);
+        bw.write(list.get(list.size()-5) + "\n");
+        bw.flush();
+    }
+}
