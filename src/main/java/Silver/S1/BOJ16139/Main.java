92c50180f72e1ac10ac40315f993b1afa4cc2a2e
= <tjalswhd1@naver.com>
2025-10-02 16:22:32 +0900

    25.10.02 문제풀이 업로드

--- src/main/java/Silver/S1/BOJ16139/Main.java
+++ src/main/java/Silver/S1/BOJ16139/Main.java
@@ -1,4 +1,4 @@
-package notyet.BOJ16139;
+package Silver.S1.BOJ16139;
 
 import java.io.*;
 import java.util.StringTokenizer;
@@ -8,22 +8,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String S = br.readLine();   // 200000자 이하
-        char[] arr = new char[S.length()];
-        for(int i=0; i<S.length(); i++){
-            arr[i] = S.charAt(i);
+        int[][] arr = new int[26][S.length()+1];
+        int index = S.charAt(0) - 'a';
+        arr[index][0] = 1;
+        for(int i=0; i<26; i++) {
+            for (int j = 0; j < S.length(); j++) {
+                char ch = (char) (i + 97);
+                if (S.charAt(j) == ch) {
+                    arr[i][j + 1] = arr[i][j] + 1;
+                } else {
+                    arr[i][j + 1] = arr[i][j];
+                }
+            }
         }
         int q = Integer.parseInt(br.readLine());
         for(int i=0; i<q; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            char ch = st.nextToken().charAt(0);
+            index = st.nextToken().charAt(0) - 97;
             int l = Integer.parseInt(st.nextToken());
             int r = Integer.parseInt(st.nextToken());
-            int count = 0;
-            for(int j=l; j<=r; j++){
-                if(arr[j] == ch){
-                    count++;
-                }
-            }
+            int count = arr[index][r+1] - arr[index][l];
             bw.write(count + "\n");
         }
         bw.flush();
