84dede6869afd412138c4d527f4c53d5764f084a
= <tjalswhd1@naver.com>
2025-10-07 20:58:33 +0900

    25.10.07 문제풀이 업로드

---
+++ src/main/java/Silver/S3/BOJ20920/Main.java
@@ -0,0 +1,59 @@
+package Silver.S3.BOJ20920;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Word implements Comparable<Word> {
+        String word;
+        int count;
+
+        public Word(String word, int count){
+            this.word = word;
+            this.count = count;
+        }
+
+        @Override
+        public int compareTo(Word o) {
+
+            if(this.count == o.count){
+                if(this.word.length() == o.word.length()){
+                    return this.word.compareTo(o.word);
+                } else {
+                    return o.word.length() - this.word.length();
+                }
+            } else {
+                return o.count - this.count;
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        HashMap<String, Integer> hm = new HashMap<>();
+        for(int i=0; i<N; i++){
+            String word = br.readLine();
+            if(word.length() >= M){
+                if(hm.containsKey(word)){
+                    hm.replace(word, hm.get(word) + 1);
+                } else {
+                    hm.put(word, 1);
+                }
+            }
+        }
+        PriorityQueue<Word> pq = new PriorityQueue<>();
+        for(String str : hm.keySet()){
+            int count = hm.get(str);
+            Word word = new Word(str, count);
+            pq.offer(word);
+        }
+        while(!pq.isEmpty()){
+            Word word = pq.poll();
+            bw.write(word.word + "\n");
+        }
+        bw.flush();
+    }
+}
