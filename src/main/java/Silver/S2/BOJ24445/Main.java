1e55a64dc590fbfee05e2ccf8a98d58955cc7e20
= <tjalswhd1@naver.com>
2025-10-21 13:13:34 +0900

    25.10.21 문제풀이 업로드

---
+++ src/main/java/Silver/S2/BOJ24445/Main.java
@@ -0,0 +1,54 @@
+package Silver.S2.BOJ24445;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int visitedNumber = 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());   // 정점의 수
+        int M = Integer.parseInt(st.nextToken());   // 간선의 수
+        int R = Integer.parseInt(st.nextToken());   // 시작 정점
+        PriorityQueue<Integer>[] connectionList = new PriorityQueue[N + 1];
+        for (int i = 1; i <= N; i++) {
+            connectionList[i] = new PriorityQueue<>(Collections.reverseOrder());
+        }
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
+            connectionList[node1].add(node2);
+            connectionList[node2].add(node1);
+        }
+        int[] visited = new int[N+1];
+        BFS(R, connectionList, visited);
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<=N; i++){
+            sb.append(visited[i] + "\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    public static void BFS(int start, PriorityQueue<Integer>[] connectionList,  int[] visited){
+        Queue<Integer> queue = new LinkedList<>();
+        visited[start] = visitedNumber;
+        visitedNumber++;
+        queue.offer(start);
+        while(!queue.isEmpty()){
+            int current = queue.poll();
+            PriorityQueue<Integer> pq = connectionList[current];
+            while(!pq.isEmpty()){
+                int next = pq.poll();
+                if(visited[next] == 0){
+                    visited[next] = visitedNumber;
+                    visitedNumber++;
+                    queue.offer(next);
+                }
+            }
+        }
+    }
+}
