ad391a4295f86edfdcfd33da7a68826c23e61374
= <tjalswhd1@naver.com>
2025-11-11 17:27:02 +0900

    25.11.11 문제풀이 업로드

---
+++ src/main/java/Bronze/B5/BOJ2743/Main.java
@@ -0,0 +1,13 @@
+package Bronze.B5.BOJ2743;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        bw.write(str.length() + "\n");
+        bw.flush();
+    }
+}
